ad5fdb7e84532776aa428276230e1f273affe96b
Gustavo <guimaraesgustavo2003@gmail.com>
2025-09-03 04:41:27 -0300

    added structure for db integration in production

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,14 +1,8 @@
-require('dotenv').config(); // carrega o .env
-
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: String(process.env.DB_PASSWORD),
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_DATABASE,
-  ssl: false
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
 module.exports = pool;
